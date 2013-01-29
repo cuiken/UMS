4e96f65ff4b3cefe1bdb54d68a7ed1a28ef4342b
ken <ken.cuik@gmail.com>
2013-01-29 10:22:21 +0800

    jpaMapping refactor

--- src/test/java/com/tp/repository/JpaMappingTest.java
+++ src/test/java/com/tp/repository/JpaMappingTest.java
@@ -1,13 +1,8 @@
 package com.tp.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.metamodel.EntityType;
-import javax.persistence.metamodel.Metamodel;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -26,32 +21,18 @@ public class JpaMappingTest extends SpringTxTestCase {
 
 	private static Logger logger = LoggerFactory.getLogger(JpaMappingTest.class);
 
-//	@PersistenceContext
-//	private EntityManager em;
-	
 	@Autowired
 	private SessionFactory sessionFactory;
 
-//	@Test
-//	public void allClassMapping() throws Exception {
-//		Metamodel model = em.getEntityManagerFactory().getMetamodel();
-//
-//		assertTrue("No entity mapping found", model.getEntities().size() > 0);
-//
-//		for (EntityType entityType : model.getEntities()) {
-//			String entityName = entityType.getName();
-//			em.createQuery("select o from " + entityName + " o").getResultList();
-//			logger.info("ok: " + entityName);
-//
-//		}
-//	}
-	
 	@Test
-	public void allClassMapping()throws Exception{
+	public void allClassMapping() throws Exception {
 		Session session = sessionFactory.openSession();
 
 		try {
 			Map metadata = sessionFactory.getAllClassMetadata();
+
+			assertTrue("No entity mapping found", metadata.values().size() > 0);
+
 			for (Object o : metadata.values()) {
 				EntityPersister persister = (EntityPersister) o;
 				String className = persister.getEntityName();
